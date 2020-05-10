253f573eee9a595584004342fb3102b3c91fbb10
Alexander <aleksandrcw@gmail.com>
2020-05-11 01:35:23 +0300

    Add TrackDetailController

---
+++ MusicPlayer/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,17 @@
+//
+//  TrackDetailView.swift
+//  MusicPlayer
+//
+//  Created by Александр Цветков on 11.05.2020.
+//  Copyright © 2020 Александр Цветков. All rights reserved.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
